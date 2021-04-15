79b2bb3370be92cb5d5478c0460901ccc44746f7
unknown <nhounhou@hotmail.com>
2021-04-14 21:31:22 -0400

    caches

--- public/service-worker.js
+++ public/service-worker.js
@@ -17,12 +17,12 @@ const FILES_TO_CACHE = [
 self.addEventListener("install", function (evt) {
   // pre cache image data
   evt.waitUntil(
-    caches.open(DATA_CACHE_NAME).then((cache) => cache.add("/api/images"))
+    caches.open(DATA_CACHE_NAME).then((cache) => caches.add("/api/images"))
   );
     
   // pre cache all static assets
   evt.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
+    caches.open(CACHE_NAME).then((cache) => caches.addAll(FILES_TO_CACHE))
   );
 
   // tell the browser to activate this service worker immediately once it
